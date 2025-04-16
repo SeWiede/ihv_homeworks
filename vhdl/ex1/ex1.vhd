5467872bcc1a586d80cdb5d717a1916f22ca46fa
Sebastian Wiedemann <sebastian.wiedemann@tuwien.ac.at>
2025-04-16 11:46:25 +0200

    ex1 done

--- vhdl/ex1/ex1.vhd
+++ vhdl/ex1/ex1.vhd
@@ -11,10 +11,30 @@ architecture behav of ex1 is
   pure function ColorToString(pos: integer) return string is
   begin
     -- Enter your code here
+    if pos < 0 or pos > ColorT'pos(ColorT'high)  then
+      Alert("Invalid pos" & integer'image(pos));
+      return "OutOfRange";
+    else
+      return to_string(ColorT'val(pos));
+    end if;
   end function;
 
   pure function ColorsToList return string is
+    type stringAccess is access string;
+    variable oldList, newList, currentElem: stringAccess;
+  begin
     -- Enter your code here
+    for i in 0 to ColorT'pos(ColorT'high) loop
+      currentElem := new string'(to_string(ColorT'val(i)));
+      if i = 0 then
+        newList := currentElem;
+      else
+        newList := new string'(oldList.all & ", " & currentElem.all);
+      end if;
+      oldList := newList;
+    end loop;
+
+    return newList.all;
   end function;
 
 begin
@@ -25,6 +45,19 @@ begin
     wait for 0 ns;
     -- Enter your code here
     Log("**********************************");
+    Log("******** COLORS TO LIST *********");
+    Log("**********************************");
+    
+    report ColorsToList;
+
+    Log("**********************************");
+    Log("**********************************");
+
+    report ColorToString(0);
+    report ColorToString(1);
+    report ColorToString(10);
+
+
     ReportAlerts;
     std.env.stop;
     wait ; 
